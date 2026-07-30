051c0854d32cc205cef8541f80df31b962cef126
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-07-30 18:08:21 +0100

    Added Time Monitor Problem

--- TopRatedRestaurant.js
+++ TopRatedRestaurant.js
@@ -13,3 +13,8 @@ function topRatedRestaurant(restaurants) {
     }
     return highRate.name.toUpperCase();
 }
+
+console.log(topRatedRestaurant([{ name: "Pasta Palace", rating: 4.5 }, { name: "Burger Barn", rating: 4.7 }, { name: "Sushi Spot", rating: 4.9 }]));
+console.log(topRatedRestaurant([{ name: "Pizza Place", rating: 4.2 }, { name: "Taco Town", rating: 4.1 }, { name: "Salad Shack", rating: 4.3 }]));
+console.log(topRatedRestaurant([]));
+console.log(topRatedRestaurant("hello"));
