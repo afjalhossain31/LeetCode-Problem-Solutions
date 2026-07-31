a8eec87ac3cbfff770eaf9f5a1fc6a05027baada
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-07-31 17:33:45 +0100

    Probelm 88 Merge Sorted

---
+++ MergeSorted.js
@@ -0,0 +1,11 @@
+var merge = function(nums1, m, nums2, n) {
+
+    for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
+    nums1.sort((a, b) => a - b);
+};
+
+
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
