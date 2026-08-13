8c55141261ff93694f6ee6a462a1d83a6cb642f7
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-13 16:18:43 +0600

    Active User Filter

---
+++ Activeuser.js
@@ -0,0 +1,19 @@
+function filterActiveUsers(users) {
+    if (!Array.isArray(users) || users.length === 0) {
+        return "Invalid";
+    }
+
+    for (const user of users) {
+        if (
+            typeof user !== "object" ||
+            user === null ||
+            !("isActive" in user)
+        ) {
+            return "Invalid";
+        }
+    }
+
+    return users.filter(user => user.isActive === true);
+}
+
+console.log(filterActiveUsers([{ name: "Abdullah", isActive: true }, { name: "Ali", isActive: false }, { name: "Ali", isActive: true }]));
