560c9b74c3b6eac10796ced09c9c805b76b22302
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-21 22:08:26 +0600

    Majority Element check

---
+++ MajoeityElement.js
@@ -0,0 +1,20 @@
+var majorityElement = function(nums) {
+
+    let candidate = nums[0];
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+};
+
+console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
