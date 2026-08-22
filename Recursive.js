1ad27fa5b90e7f25a91f29cdd2ec6305de446b6f
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-22 21:17:44 +0600

    Maximum Depth of Binary Tree

---
+++ Recursive.js
@@ -0,0 +1,34 @@
+var maxDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+
+    return Math.max(left, right) + 1;
+};
+console.log(maxDepth(root));
+
+// Example usage:
+// Assuming you have a TreeNode class defined as follows:
+// class TreeNode {
+//     constructor(val, left = null, right = null) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+// Create the tree: [3, 9, 20, null, null, 15, 7]
+//       3
+//      / \
+//     9  20
+//       /  \
+//      15   7
+// let root = new TreeNode(3);
+// root.left = new TreeNode(9);
+// root.right = new TreeNode(20);
+// root.right.left = new TreeNode(15);
+// root.right.right = new TreeNode(7);
+
