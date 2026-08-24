17350237df35b2c18b0fc9ea23816c3f913cb2fb
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-25 02:15:02 +0600

    Added Array  Operation and Array

---
+++ Foundation_Program_JS/array.js
@@ -0,0 +1,19 @@
+// array
+let name = ["Afjal", "Bin", "Hazi", "Abdul", "Salam"];
+
+let number = [100, 200, 250, 300, 600];
+
+// console.log(name[0], number[5]);
+
+// name.push("Abdullah");
+// name.push("raki");
+
+// name.pop()
+
+// name.unshift("Rakin");
+// name.shift();
+
+//splice(startindex)
+
+name.splice(2, 1, "Afjal", "Hazi");
+console.log(name);
